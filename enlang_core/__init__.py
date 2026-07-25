da1be4c29da37a4f8b2fecdbce33ddf8e04f9f35
Wolvestorm11 <notaerowhy@gmail.com>
2026-07-25 16:07:38 +0530

    Prepare version 2.0.3 release with database execution engine for PyPI

--- enlang_core/__init__.py
+++ enlang_core/__init__.py
@@ -6,5 +6,5 @@ Natural English Programming Language Transpiler, Interpreter & Domain Helpers.
 from .transpiler import EnLangTranspiler
 from .interpreter import EnLangInterpreter
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 __all__ = ["EnLangTranspiler", "EnLangInterpreter"]
